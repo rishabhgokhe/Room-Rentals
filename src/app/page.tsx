5537d129789425e62839b32a9ca677bf96708aa5
Rishabh Gokhe <rishabhgokhe2004@gmail.com>
2024-06-20 20:48:58 +0530

    added navigation bar and landing text component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { NextTemplate } from "../components/UI/NextTemplate";
+import { GridBackground } from "@/components/UI/BackgroundGrid/BackgroundGrid";
 
 export default function Home() {
   return <>
-  <NextTemplate />
+  <GridBackground />
+  {/* <NextTemplate /> */}
   <h1 style={{fontSize: 200, fontWeight: "bold"}}>Rishabh</h1>
   </>;
 }
